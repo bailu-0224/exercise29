0e5d4b55003b6c7caf49feb269ea10076691f96a
bailu-0224 <522141927@qq.com>
2020-09-01 15:51:14 +0800

    添加了viewController，修改了前端界面

--- src/main/java/com/huadi/exercise29/controller/LoginController.java
+++ src/main/java/com/huadi/exercise29/controller/LoginController.java
@@ -1,13 +1,26 @@
 package com.huadi.exercise29.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.Map;
 
 @Controller
 public class LoginController {
-    @RequestMapping({"/","/login"})
-    public String login()
+    @PostMapping(value="/user/login")
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        Map<String,Object> map)
     {
-        return "login";
+
+        if(!StringUtils.isEmpty(username) && "123456".equals(password))
+            return "fire/index";
+        else {
+            map.put("msg", "用户名密码错误");
+            return "fire/login";
+        }
     }
 }
