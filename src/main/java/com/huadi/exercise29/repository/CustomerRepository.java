844ddb5f0f4749aecb0ebde0dcaf6b315c456854
YYorozuya <751924395@qq.com>
2020-09-03 13:22:22 +0800

    鉴定类更新一系列数据访问操作

--- src/main/java/com/huadi/exercise29/repository/CustomerRepository.java
+++ src/main/java/com/huadi/exercise29/repository/CustomerRepository.java
@@ -8,19 +8,19 @@ import org.springframework.stereotype.Repository;
 @Mapper
 public interface CustomerRepository {
 
-    //插入一个用户
+    //插入一个用户，插入成功返回1，失败返回0
     int insert(Customer customer);
 
-    //删除一个用户
+    //删除一个用户，成功返回1，用户不存在或删除失败返回0
     int delete(String account);
 
-    //通过账户名查找一个用户,如果用户不存在，返回 null
+    //通过账户名查找一个用户，如果用户不存在，返回 null
     Customer selectByAccount(String account);
 
     //通过账户名查看一个用户的密码
     String checkPasswd(String account);
 
-    //将一个用户的权限改为0（封禁）
+    //将一个用户的权限改为0（封禁），成功返回1，失败返回0
     int ban(String account);
 
     //将一个用户的权限改为1
