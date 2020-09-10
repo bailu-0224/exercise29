9b9b347596bd4782431a3bb46a15aa9e3de62ac6
AminJK <846468424@qq.com>
2020-09-10 22:06:38 +0800

    新增鉴定管理

--- src/main/java/com/huadi/exercise29/controller/mangementcontroller/AusmanagementControll.java
+++ src/main/java/com/huadi/exercise29/controller/mangementcontroller/AusmanagementControll.java
@@ -2,11 +2,11 @@ package com.huadi.exercise29.controller.mangementcontroller;
 
 import com.huadi.exercise29.entity.Authentication;
 import com.huadi.exercise29.repository.AuthenticationRepository;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,8 +15,15 @@ public class AusmanagementControll {
     @Autowired
     private AuthenticationRepository au;
 
-    @RequestMapping("/ausmanage")
-    public String listAu(@RequestParam("userID")String ID, Model model){
+    @GetMapping("/ausmanage")
+    public  String ausmanage(Model model){
+        List<Authentication> authenticationList = au.selectAll();
+        model.addAttribute("aus",authenticationList);
+        return "/manage/admin-table";
+    }
+
+    @RequestMapping("/ausmanage/search")
+    public String ausSearch(@RequestParam("userID") String ID, Model model){
         if(ID.isEmpty()) {
             List<Authentication> authenticationList = au.selectAll();
             model.addAttribute("aus", authenticationList);
@@ -26,5 +33,11 @@ public class AusmanagementControll {
             model.addAttribute("aus",authenticationList);
             return "/manage/admin-table";
         }
+    }
+
+    @DeleteMapping("/ausmanage/delete")
+    public String ausdelete(@RequestParam("deleteID") String ID){
+        au.deleteById(Integer.parseInt(ID));
+        return "redirect:/manage/admin-table";
     }
 }
